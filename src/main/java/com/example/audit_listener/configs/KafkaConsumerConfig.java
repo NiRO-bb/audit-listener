df3dcf3c090adefcb047bc0c04cfc45bf01f899f
Nikita Borisov <nikita.borisov44444@gmail.com>
2025-08-23 15:35:37 +0400

    feat: add Kafka producer
    add producer to send error messages

--- src/main/java/com/example/audit_listener/configs/KafkaConsumerConfig.java
+++ src/main/java/com/example/audit_listener/configs/KafkaConsumerConfig.java
@@ -10,18 +10,16 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Defines based settings of application.
- * Provides some beans for application.
+ * Sets configiration for Kafka consumer.
  */
 @Configuration
 @EnableKafka
-public class ApplicationConfig {
+public class KafkaConsumerConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapAddress;
@@ -34,7 +32,7 @@ public class ApplicationConfig {
      * Sets some settings: StringDeserializer for key,
      * JsonDeserializer for value, disable offset auto commit.
      * Uses JsonDeserializer type mapping:
-     * 'http' for KafkaHttpLog class, 'annotation' for KafkaAnnotationLog class.
+     * 'http' for KafkaRequestLog class, 'annotation' for KafkaMethodLog class.
      *
      * @return
      */
@@ -44,12 +42,8 @@ public class ApplicationConfig {
         configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         configs.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-        configs.put(JsonDeserializer.TYPE_MAPPINGS,
-                "http:com.example.audit_listener.dto.KafkaHttpLog, " +
-                        "annotation:com.example.audit_listener.dto.KafkaAnnotationLog");
-        configs.put(JsonDeserializer.TRUSTED_PACKAGES, "com.example.audit_listener.dto");
         return new DefaultKafkaConsumerFactory<>(configs);
     }
 
